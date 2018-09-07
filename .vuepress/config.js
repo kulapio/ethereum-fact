afdc5c7c71d8db1a02e3569bea7a7d9263fa865b
Nattapon Nimakul <totizy@gmail.com>
2018-09-07 21:28:48 +0700

    add home

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -1,19 +1,20 @@
 module.exports = {
   title: 'Ethereum Fact',
-  description: 'Ethereum Fact',
+  description: 'Every fact on ethereum ecosystem',
   markdown: {
     lineNumbers: true
   },
   base: '/ethereum/fact/',
   themeConfig: {
+    displayAllHeaders: true,
     nav: [
-      { text: 'Ethereum Fact', link: '/' },
+      { text: 'Fact', link: '/fact/' },
       { text: 'Run node', link: '/runnode/' },
       { text: 'Raiden network', link: '/raiden/' },
       { text: 'Github', link: 'https://github.com/kulapio/EthereumFact.git' },
     ],
     sidebar: [
-      '/',
+      ['/fact/', 'Fact'],
       ['/runnode/', 'Run node'],
       ['/raiden/', 'Raiden network']
     ]
