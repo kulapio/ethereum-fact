cdcdaf657289d55a0ee5fe5cca7c340fd44aa8d7
Nattapon Nimakul <totizy@gmail.com>
2018-09-15 11:29:52 +0700

    add wallet/

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -11,12 +11,14 @@ module.exports = {
       { text: 'Fact', link: '/fact/' },
       { text: 'Run node', link: '/runnode/' },
       { text: 'Raiden network', link: '/raiden/' },
+      { text: 'Wallet', link: '/wallet/' },
       { text: 'Github', link: 'https://github.com/kulapio/EthereumFact.git' },
     ],
     sidebar: [
       ['/fact/', 'Fact'],
       ['/runnode/', 'Run node'],
-      ['/raiden/', 'Raiden network']
+      ['/raiden/', 'Raiden network'],
+      ['/wallet/', 'Wallet']
     ]
   }
 }
