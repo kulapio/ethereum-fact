cbcd23b9c8d18890cb89dd5e552b0722091c016f
Nattapon Nimakul <totizy@gmail.com>
2018-09-19 17:30:23 +0700

    move raiden network nevigation bar

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -10,14 +10,12 @@ module.exports = {
     nav: [
       { text: 'Fact', link: '/fact/' },
       { text: 'Run node', link: '/runnode/' },
-      { text: 'Raiden network', link: '/raiden/' },
       { text: 'Wallet', link: '/wallet/' },
       { text: 'Github', link: 'https://github.com/kulapio/EthereumFact.git' },
     ],
     sidebar: [
       ['/fact/', 'Fact'],
       ['/runnode/', 'Run node'],
-      ['/raiden/', 'Raiden network'],
       ['/wallet/', 'Wallet']
     ]
   }
