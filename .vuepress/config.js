d5ed0345d6fb57a442417278a9489185e75a69d0
Nattapon Nimakul <totizy@gmail.com>
2018-09-19 17:39:18 +0700

    add Dev navigation bar

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -9,12 +9,14 @@ module.exports = {
     displayAllHeaders: true,
     nav: [
       { text: 'Fact', link: '/fact/' },
+      { text: 'Dev', link: '/dev/' },
       { text: 'Node', link: '/runnode/' },
       { text: 'Wallet', link: '/wallet/' },
       { text: 'Github', link: 'https://github.com/kulapio/EthereumFact.git' },
     ],
     sidebar: [
       ['/fact/', 'Fact'],
+      ['/dev/', 'Dev'],
       ['/runnode/', 'Node'],
       ['/wallet/', 'Wallet']
     ]
